548fc77c92b7381c73c353c14efc2941af5e391e
Siddharth Borderwala <siddharthborderwala@gmail.com>
2022-01-22 02:47:47 +0530

    plugins refactor

--- src/plugins/redis/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { Tedis } from 'tedis';
-
-// Use TypeScript module augmentation to declare the type of server.redis to be Tedis
-declare module 'fastify' {
-  interface FastifyInstance {
-    redis: Tedis;
-  }
-}
