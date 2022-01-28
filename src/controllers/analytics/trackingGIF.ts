20f487a710d50607dc73258d5958cf903da0f33b
Aman Yadav <42231378+yadav-aman@users.noreply.github.com>
2022-01-28 13:36:45 +0530

    Add ua parser

--- src/controllers/analytics/trackingGIF.ts
+++ src/controllers/analytics/trackingGIF.ts
@@ -1,4 +1,5 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+import { createStatistics } from '../../util/createStatistics';
 
 const BEACON = Buffer.from(
   'R0lGODlhAQABAIAAANvf7wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==',
@@ -15,22 +16,8 @@ export const trackingGIF =
   (fastify: FastifyInstance) =>
   async (request: FastifyRequest, reply: FastifyReply) => {
     const { url, ref, t } = request.query as QueryType;
+    createStatistics(request);
     if (url) {
-      const data = {
-        url: url,
-        title: t || '',
-        referer: ref || 'direct',
-        referer2: request.headers.referer || 'direct',
-        cookies: request.cookies || request.headers.cookies || {},
-        path: request.headers.path || '',
-        host: request.headers.host || '',
-        ip: request.ip,
-        parmas: request.params,
-        platform: request.headers['sec-ch-ua-platform'],
-      };
-
-      console.log(data);
-
       reply.header('Content-Type', 'image/gif');
       reply.header('Cache-Control', 'private, no-cache');
       reply.status(200).send(BEACON);
