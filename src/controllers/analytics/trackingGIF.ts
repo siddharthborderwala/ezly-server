3fd1974e343ff4e75ffdb8d7e9d12211e573ae03
Aman Yadav <42231378+yadav-aman@users.noreply.github.com>
2022-01-28 12:14:38 +0530

    Add analytics functionality

--- src/controllers/analytics/trackingGIF.ts
+++ src/controllers/analytics/trackingGIF.ts
@@ -7,14 +7,30 @@ const BEACON = Buffer.from(
 
 type QueryType = {
   url?: string;
+  t?: string;
+  ref?: string;
 };
 
 export const trackingGIF =
   (fastify: FastifyInstance) =>
   async (request: FastifyRequest, reply: FastifyReply) => {
-    const { url } = request.query as QueryType;
-
+    const { url, ref, t } = request.query as QueryType;
     if (url) {
+      const data = {
+        url: url,
+        title: t || '',
+        referer: ref || 'direct',
+        referer2: request.headers.referer || 'direct',
+        cookies: request.cookies || request.headers.cookies || {},
+        path: request.headers.path || '',
+        host: request.headers.host || '',
+        ip: request.ip,
+        parmas: request.params,
+        platform: request.headers['sec-ch-ua-platform'],
+      };
+
+      console.log(data);
+
       reply.header('Content-Type', 'image/gif');
       reply.header('Cache-Control', 'private, no-cache');
       reply.status(200).send(BEACON);
