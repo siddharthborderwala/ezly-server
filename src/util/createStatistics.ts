cc850c0efc09ce9a6baf4072c204e23b649eef79
Aman Yadav <42231378+yadav-aman@users.noreply.github.com>
2022-02-06 16:25:20 +0530

    Add analytics logic

--- src/util/createStatistics.ts
+++ src/util/createStatistics.ts
@@ -1,5 +1,6 @@
-import { FastifyRequest } from 'fastify';
+import { FastifyRequest } from 'fastify/types/request';
 import { UAParser } from 'ua-parser-js';
+import fetch from 'cross-fetch';
 
 export const createStatistics = async (request: FastifyRequest) => {
   const ua = UAParser(request.headers['user-agent']);
@@ -7,16 +8,41 @@ export const createStatistics = async (request: FastifyRequest) => {
   const data = {
     referer: request.headers.referer || 'direct',
     path: request.headers.path || '',
-    // host: request.headers.host || '',
-    // parmas: request.params,
     ip: request.ip,
     browser: ua.browser.name || '',
+    browserLang: request.headers['accept-language']?.split(',')[0] || '',
     os: ua.os.name || '',
     osVer: ua.os.version || '',
     device: ua.device.vendor || '',
     deviceModel: ua.device.model || '',
     deviceType: ua.device.type || '',
+    countryCode: '',
+    countryName: '',
   };
+
+  try {
+    const response = await fetch(`https://ip2c.org/${data.ip}`).then((res) =>
+      res.text()
+    );
+
+    const [
+      status,
+      countryCodeTwoLetters,
+      countryCodeThreeLetters,
+      countryName,
+    ] = response.split(';');
+
+    if (status === '1') {
+      data.countryCode = countryCodeTwoLetters;
+      data.countryName = countryName;
+    } else {
+      data.countryCode = data.countryName = 'unknown';
+    }
+  } catch (error) {
+    data.countryCode = data.countryName = 'unknown';
+  }
+
+  // TODO acccess linkID and then store to prisma
 
   console.log(data);
 };
