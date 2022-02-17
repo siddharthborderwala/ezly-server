7442a7527242592e9ea5815e87f9831539f4f759
Shagun Agrawal <agrawalshagun07@gmail.com>
2022-02-18 00:00:30 +0530

    image upload router

---
+++ src/util/s3-client.ts
@@ -0,0 +1,11 @@
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+
+const client = new S3Client({
+  credentials: {
+    accessKeyId: process.env.AWS_S3_CLIENT_ID as string,
+    secretAccessKey: process.env.AWS_S3_SECRET_KEY as string,
+  },
+  region: 'ap-south-1',
+});
+
+export default client;
