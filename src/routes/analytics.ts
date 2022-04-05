551ae7314904f4a1ab2c0aeff149e7a0f7afdcbd
Aman Yadav <42231378+yadav-aman@users.noreply.github.com>
2022-04-05 15:12:02 +0530

    Revert "Update: removed gif and js tracking, add get analytics route"
    This reverts commit b597f1019b945906c5a67a18615ad6a12691a48c.

--- src/routes/analytics.ts
+++ src/routes/analytics.ts
@@ -1,11 +1,8 @@
 import { FastifyInstance } from 'fastify';
-import { getStats } from '../controllers/analytics';
 
 const AnalyticsRoute = async (
   fastify: FastifyInstance,
   options: Record<any, any>
-) => {
-  fastify.get('/:linkId', getStats(fastify));
-};
+) => {};
 
 export default AnalyticsRoute;
