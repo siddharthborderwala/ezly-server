e8660426a6adab81bb7cf6ebcf2d16a7a92a170f
Aman Yadav <42231378+yadav-aman@users.noreply.github.com>
2022-04-05 17:57:45 +0530

    Imporved get analytics route

--- src/controllers/analytics/get.ts
+++ src/controllers/analytics/get.ts
@@ -1,26 +1,52 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 
+type GetresParamsType = {
+  alias: string;
+};
+
 export const getStats =
   (fastify: FastifyInstance) =>
   async (request: FastifyRequest, reply: FastifyReply) => {
-    const { alias }: any = request.params;
+    const { alias } = request.params as GetresParamsType;
     try {
-      const stats = await fastify.prisma.link.findFirst({
+      const res = await fastify.prisma.link.findFirst({
         where: {
           short_url: alias,
         },
         include: {
           analytics: true,
+          collection: true,
         },
       });
-      // console.log(stats);
-      if (!stats) {
+      if (!res) {
         reply.status(404).send('Data not found');
       }
+
+      const stats = {
+        url: res?.url,
+        alias: res?.short_url,
+        clicks: res?.analytics.length,
+        collection: res?.collection.name,
+        analytics: res?.analytics.map((item: any) => ({
+          id: item.id,
+          referer: item.referer,
+          path: item.path,
+          browser: item.browser,
+          browserLang: item.browserLang,
+          os: item.os,
+          osVer: item.osVer,
+          device: item.device,
+          deviceModel: item.deviceModel,
+          deviceType: item.deviceType,
+          countryCode: item.countryCode,
+          countryName: item.countryName,
+        })),
+      };
+
       return reply.status(200).send({
         stats,
       });
     } catch (err) {
-      return reply.badRequest('error while retreiving stats');
+      return reply.badRequest('error while retreiving res');
     }
   };
